663ee3d8af013353c7799021c424840951afb23d
infinitytec <infinitytec@users.noreply.github.com>
2025-09-30 09:19:49 -0500

    Create const.py

---
+++ custom_components/openweathermap_all/const.py
@@ -0,0 +1,5 @@
+DOMAIN = "openweathermap_air_quality"
+DEFAULT_NAME = "OpenWeatherMap Air Quality"
+DEFAULT_LANGUAGE = "en"
+ATTRIBUTION = "Data provided by OpenWeatherMap"
+MANUFACTURER = "OpenWeather
