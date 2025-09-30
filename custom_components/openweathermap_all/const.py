285406b5247c31daaa0c0e8fc7fa96dcbb414e03
infinitytec <infinitytec@users.noreply.github.com>
2025-09-30 09:26:20 -0500

    Update const.py

--- custom_components/openweathermap_all/const.py
+++ custom_components/openweathermap_all/const.py
@@ -2,4 +2,10 @@ DOMAIN = "openweathermap_air_quality"
 DEFAULT_NAME = "OpenWeatherMap Air Quality"
 DEFAULT_LANGUAGE = "en"
 ATTRIBUTION = "Data provided by OpenWeatherMap"
-MANUFACTURER = "OpenWeather
+MANUFACTURER = "OpenWeather"
+
+
+# HA constants
+CONF_API_KEY = "api_key"
+CONF_LATITUDE = "latitude"
+CONF_LONGITUDE = "longitude"
