b176767b6470b558a1a559c264d425aae535f640
infinitytec <infinitytec@users.noreply.github.com>
2025-09-30 09:04:25 -0500

    Update config_flow.py

--- custom_components/openweathermap_all/config_flow.py
+++ custom_components/openweathermap_all/config_flow.py
@@ -1,23 +1,29 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_API_KEY, CONF_LATITUDE, CONF_LONGITUDE
-from . import DOMAIN
 
-class Owm2JsonConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for OWM2JSON."""
+from .const import DOMAIN
 
+class OpenWeatherMapAQConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         errors = {}
         if user_input is not None:
-            # TODO: Optionally test API connectivity here
-            return self.async_create_entry(title="OWM Air Quality", data=user_input)
+            # Here you could validate the API key by hitting OpenWeatherMap
+            return self.async_create_entry(
+                title="OpenWeatherMap Air Quality",
+                data=user_input
+            )
 
-        data_schema = vol.Schema({
+        schema = vol.Schema({
             vol.Required(CONF_API_KEY): str,
-            vol.Required(CONF_LATITUDE): str,
-            vol.Required(CONF_LONGITUDE): str,
+            vol.Required(CONF_LATITUDE, default=self.hass.config.latitude): float,
+            vol.Required(CONF_LONGITUDE, default=self.hass.config.longitude): float,
         })
 
-        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema,
+            errors=errors,
+        )
